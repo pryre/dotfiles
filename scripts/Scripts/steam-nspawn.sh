c8437447924e7cb0b2e1c93520c88a30eb0d1b41
Kye Morton <pryre.dev@outlook.com>
2018-03-03 22:51:03 +1000

    Added note to share X screens with nspawn

--- scripts/Scripts/steam-nspawn.sh
+++ scripts/Scripts/steam-nspawn.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+xhost +local:
 systemctl start systemd-nspawn@steamcontainer.service
 machinectl shell pryre@steamcontainer /usr/bin/steam
 systemctl stop systemd-nspawn@steamcontainer.service
