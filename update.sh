873b1ecd4ba5145292685b6f795f28876ae04d80
pryre <kyemorton@hotmail.com>
2018-03-05 11:26:40 +1000

    Better updating method with fetch/merge

--- update.sh
+++ update.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
-OLD_DIRS=$(ls -d */ | tr -d '/')
-echo "Removing links for:"
-echo "$OLD_DIRS"
-stow -D $OLD_DIRS
 
-echo ""
 echo "Updating dotfiles:"
-git pull
-echo ""
+git fetch
 
-NEW_DIRS=$(ls -d */ | tr -d '/')
-echo "Adding links for:"
-echo "$NEW_DIRS"
-stow $NEW_DIRS
+if [ $? -eq 0 ];
+then
+	echo ""
+    OLD_DIRS=$(ls -d */ | tr -d '/')
+	echo "Removing links for:"
+	echo "$OLD_DIRS"
+	stow -D $OLD_DIRS
+	echo ""
+
+	echo "Merging changes:"
+	git merge
+	echo ""
+
+	NEW_DIRS=$(ls -d */ | tr -d '/')
+	echo "Adding links for:"
+	echo "$NEW_DIRS"
+	stow $NEW_DIRS
+else
+    echo "Unable to fetch updates from the remote repository"
+fi
+
