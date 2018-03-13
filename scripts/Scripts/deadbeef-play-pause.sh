e70ee7981359707461391e0ce67508d57120760a
Kye Morton <pryre.dev@outlook.com>
2018-03-13 12:14:46 +1000

    Better music check for deadbeef file browser?

--- scripts/Scripts/deadbeef-play-pause.sh
+++ scripts/Scripts/deadbeef-play-pause.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-if pgrep "deadbeef" > /dev/null
+if [ -e ~/Music ]
 then
 	deadbeef --play-pause
+else
+	ERROR_TIME=2000
+	ERROR_IMG=~/.icons/la-capitaine-icon-theme/actions/symbolic/action-unavailable-symbolic.svg
+	notify-send -t $ERROR_TIME -i $ERROR_IMG "Music directory could not be accessed"
 fi
