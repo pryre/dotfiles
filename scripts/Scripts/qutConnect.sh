ded7bfde9d4503fd7d7eb64c19363bb84fe65d45
Kye Morton <pryre.dev@outlook.com>
2018-03-16 22:28:04 +1000

    updates to scripts

--- scripts/Scripts/qutConnect.sh
+++ scripts/Scripts/qutConnect.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 read -p "Username: " VPN_USER
 read -s -p "Password: " VPN_PASS
+#PIDFILE=/run/openconnect_${Interface}.pid
 echo
 
-echo -n $VPN_PASS | sudo openconnect -u $VPN_USER --passwd-on-stdin sas.qut.edu.au &
+echo -n $VPN_PASS | sudo openconnect -u $VPN_USER --passwd-on-stdin sas.qut.edu.au
 #sudo openconnect sas.qut.edu.au
 
-VPN_PID=$!
+#echo "Redirecting all traffic over tunnel"
+#sudo route add -net 0.0.0.0/0 tun0
 
-echo "VPN started on PID $VPN_PID"
+#read -p "Press any key to close..."
 
-echo "Redirecting all traffic over tunnel"
-sudo route add -net 0.0.0.0/0 tun0
-
-read -p "Press any key to close..."
-
-echo "Killing VPN"
-kill $VPN_PID
-
+#echo "Killing VPN"
+#kill -INT $(cat ${PIDFILE})
