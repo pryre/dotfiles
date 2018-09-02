bce445bedc9b2f39f105fe05ee7a7bbbf99d98a2
Kye Morton <pryre.dev@outlook.com>
2018-09-02 22:16:29 +1000

    updated theme and network script

--- scripts/Scripts/networkMonitor.sh
+++ scripts/Scripts/networkMonitor.sh
@@ -1,60 +1,66 @@
 #!/bin/sh
 
-DEVICE=$(ifconfig | grep "$1") #Get list of broadcast devices
+DEVICE=$1
+CONN_STATUS=$(ip addr | grep "$DEVICE" | grep 'UP')
 
 PROFILE=$2
 if [ -z "$PROFILE" ];
 then
-	#Select the first profile to use
-	PROFILE=$(netctl list | grep "$1" | head -n1 | tr -d '*')
+	PROFILE_ACT=$(netctl-auto list | grep "*" | tr -d '*')
+	if [ -z "$PROFILE" ];
+	then
+		PROFILE_ACT="[disconnected]"
+	fi
 
-	#Strip the star if it exists
-	#PROFILE_STAR=$(echo "$PROFILE" | cut -d' ' -f2)
-	#if [ -n "$PROFILE_STAR" ];
-	#then
-	#	PROFILE=$PROFILE_STAR
-	#fi
+	PROFILE="[auto] $PROFILE_ACT"
+
+	if [ -z "$CONN_STATUS" ];
+	then #Connection is not running
+		ON_CLICK="<click>systemctl start netctl-auto@$1</click>"
+	else #Connection up and running
+		ON_CLICK="<click>systemctl stop netctl-auto@$1</click>"
+	fi
+else
+	if [ -z "$CONN_STATUS" ];
+	then #Connection is not running
+		ON_CLICK="<click>netctl start $PROFILE</click>"
+	else #Connection up and running
+		ON_CLICK="<click>netctl stop $PROFILE</click>"
+	fi
 fi
 
+# Set the default icon (disconnected/doesn't exist)
+IMG_PATH="<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wired-disconnected.svg</img>"
+TOOLTIP_PRE="Status"
+
 case "$DEVICE" in
-	#Case: the device is a hardwire device
-	"eno"* | "eth"* | "usb"*)
-		echo -e "<tool>Wired status for $1\nProfile: $PROFILE</tool>"
+	"eno"* | "eth"* | "usb"*) #Case: the device is a hardwire device
+		TOOLTIP_PRE="Wired status"
 
-		CONN=$(echo "$DEVICE" | grep 'RUNNING')
-		echo $CONN
-		if [ -z "$CONN" ];
-		then
-			#Connection is not running
-			echo "<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wired-acquiring.svg</img>"
-		else
-			#Connection up and running
-			echo "<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wired-symbolic.svg</img>"
+		if [ -z "$CONN_STATUS" ];
+		then #Connection is not running
+			IMG_PATH="<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wired-acquiring.svg</img>"
+		else #Connection up and running
+			IMG_PATH="<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wired-symbolic.svg</img>"
 		fi
 
-		echo "<click>netctl stop $PROFILE</click>"
 	;;
-	#Case: the device is a wireless device
-	"wl"*)
-		PROFILE_ACT=$(netctl-auto list | grep "*" | tr -d '*')
-		echo -e "<tool>Wireless status for $1\nProfile: $PROFILE_ACT</tool>"
+	"wl"*) #Case: the device is a wireless device
+		TOOLTIP_PRE="Wireless status"
 
-		CONN=$(echo "$DEVICE" | grep 'RUNNING')
-		if [ -z "$CONN" ];
-		then
-			#Connection is not running
-			echo "<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wireless-connected-25.svg</img>"
-			echo "<click>netctl-auto enable-all</click>"
-		else
-			#Connection up and running
-			echo "<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wireless-connected-100.svg</img>"
-			echo "<click>netctl-auto disable-all</click>"
+		if [ -z "$CONN_STATUS" ];
+		then #Connection is not running
+			IMG_PATH="<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wireless-connected-25.svg</img>"
+		else #Connection up and running
+			IMG_PATH="<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wireless-connected-100.svg</img>"
 		fi
 	;;
-	#Case: the device could not be found
-	*)
-		echo -e "<tool>Status for $1\nProfile: $PROFILE</tool>"
-		echo "<img>/home/pryre/.icons/la-capitaine-icon-theme/devices/scalable/network-wired-disconnected.svg</img>"
-		echo "<click>netctl start $PROFILE</click>"
+	*) #Case: the device type could not be found
 	;;
 esac
+
+TOOLTIP="<tool>$TOOLTIP_PRE for $1\nProfile: $PROFILE</tool>"
+
+echo -e "$TOOLTIP"
+echo "$IMG_PATH"
+echo "$ON_CLICK"
