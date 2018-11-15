5427a305f773a660431a42babc21d36d80f9cccc
Kye Morton <pryre.dev@outlook.com>
2018-11-15 13:34:49 +1000

    Video recoder

---
+++ scripts/Scripts/recode_mp4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script will open all video files passed to it, and recode them as .mp4 with "good" settings
+
+CMD='ffmpeg -i $var -crf 18 -c:a copy -pix_fmt yuv420p recode_$(basename "$var" | cut -f1 -d'.').mp4'
+
+for var in "$@"
+do
+	echo "Proccessing: $var"
+	echo
+	eval $CMD
+done
