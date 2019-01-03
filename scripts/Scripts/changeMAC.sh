e8d908f23bf4e5ba5dd819e412785e19c9e1b50c
Kye Morton <pryre.dev@outlook.com>
2019-01-03 10:16:40 +1000

    laptop changes

--- scripts/Scripts/changeMAC.sh
+++ scripts/Scripts/changeMAC.sh
@@ -9,11 +9,11 @@ hexchars="0123456789ABCDEF"
 end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
 
 #Attach key to known manufacturer's ID
-whole=D8:FC:93$end
+whole=00:F4:8D$end
 
 echo Stetting MAC to: $whole
-ip link set wlp2s0 down
-ip link set wlp2s0 address $whole
+ip link set wlan0 down
+ip link set wlan0 address $whole
 
 #echo Enabling WiFi adapter...
 #ip link set wlp2s0 up
