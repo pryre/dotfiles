fddf94ed80216463f2771cca0f15ea68621dbd84
Kye Morton <pryre.dev@outlook.com>
2019-01-03 10:45:18 +1000

    Better update script

--- update.sh
+++ update.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 
-echo "Updating dotfiles:"
+echo "Updating dotfiles..."
 git fetch
 
-if [ $? -eq 0 ];
+if [ $? -eq 0 ]
 then
-	echo ""
-    OLD_DIRS=$(ls -d */ | tr -d '/')
-	echo "Removing links for:"
-	echo "$OLD_DIRS"
-	stow -D $OLD_DIRS
-	echo ""
+	if [ "$(git branch -v | grep -E '\[behind [0-9]*\]')" ]
+	then
+		echo ""
+    	OLD_DIRS=$(ls -d */ | tr -d '/')
+		echo "Removing links for:"
+		echo "$OLD_DIRS"
+		stow -D $OLD_DIRS
+		echo ""
 
-	echo "Merging changes:"
-	git merge
-	echo ""
+		echo "Merging changes:"
+		git merge
+		echo ""
 
-	NEW_DIRS=$(ls -d */ | tr -d '/')
-	echo "Adding links for:"
-	echo "$NEW_DIRS"
-	stow $NEW_DIRS
+		NEW_DIRS=$(ls -d */ | tr -d '/')
+		echo "Adding links for:"
+		echo "$NEW_DIRS"
+		stow $NEW_DIRS
+
+		gtk-update-icon-cache ~/.icons/la-capitaine-icon-theme
+	else
+		echo "Dotfiles are up to date!"
+	fi
+
+	if [ "$(git status --porcelain --ignore-submodules)" ]
+	then
+		echo "Repository contains untracked changes, consider updating"
+	fi
 else
     echo "Unable to fetch updates from the remote repository"
 fi
-
