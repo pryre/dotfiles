432cef07ea06dbeb5bde4c82acbd723400c224e4
Kye Morton <pryre.dev@outlook.com>
2019-02-27 10:03:44 +1000

    Trying to get train wifi working still

--- scripts/Scripts/vncStart.sh
+++ scripts/Scripts/vncStart.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-echo Openning x0vncserver
-x0vncserver -display :0 -passwordfile ~/.vnc/passwd
+
+echo Openning x0vncserver on IPs:
+ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}'
+
+x0vncserver \
+	-display :0 \
+	-rfbauth ~/.vnc/passwd
