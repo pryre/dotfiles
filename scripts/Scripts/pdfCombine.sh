55f5272d0cd64203963a67e7da21a4c0b99682dc
Kye Morton <pryre.dev@outlook.com>
2019-03-07 10:26:18 +1000

    Better naming for extract and easier combining spec

--- scripts/Scripts/pdfCombine.sh
+++ scripts/Scripts/pdfCombine.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
-if [ "$#" -eq 2 ]
+if [ "$#" -gt 1 ]
 then
-	gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=$1 $2*
+	F_OUT=$1
+	F_IN=""
+
+	for VAR in "$@"
+	do
+		if [ ${VAR} != ${F_OUT} ]
+		then
+			F_IN="${F_IN} ${VAR}"
+		fi
+	done
+
+	F_IN_SORT=$(echo "${F_IN}" | tr ' ' '\n' | sort -b | tr '\n' ' ')
+
+	#echo "${F_IN_SORT}"
+	gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=${F_OUT} ${F_IN}
 else
-	echo "Usage: combinePDF.sh /output/File.pdf /input/directory/"
+	echo "Usage: combinePDF.sh /output/File.pdf [ /input/file.pdf ... ]"
 	echo "Or: combinePDF.sh /output/File.pdf /input/files"
 	echo "Second option will match all files with the * wildcard"
 fi
