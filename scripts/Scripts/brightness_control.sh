12612d7e4e6c227124086fa42cb9f7084ebf87f0
Kye Morton <pryre.dev@outlook.com>
2019-04-02 12:35:55 +1000

    More functionality

--- scripts/Scripts/brightness_control.sh
+++ scripts/Scripts/brightness_control.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-if [ $# -eq 2 ]
-then
+show_help() {
+	echo "Usage: brightness_control DEVICE COMMAND"
+	echo "Commands:"
+	echo "    step [+/-] STEP"
+	echo "    get_percent"
+	echo "Make sure to set udev rules to allow users of video group to control backlight:"
+	echo "    /etc/udev/rules.d/90-backlight.rules"
+	echo '    SUBSYSTEM=="backlight", ACTION=="add", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"'
+}
+
+do_get_percent() {
+	DEVICE=$1
+
+	B="$(cat /sys/class/backlight/${DEVICE}/brightness)"
+	MB="$(cat /sys/class/backlight/${DEVICE}/max_brightness)"
+
+	echo "$(((100*B)/MB))"
+}
+
+do_step() {
 	DEVICE=$1
 	STEP=$2
 
@@ -20,9 +38,31 @@ then
 	fi
 
 	echo "$NB" > /sys/class/backlight/${DEVICE}/brightness
+}
+
+if [ $# -ge 1 ]
+then
+	if [ $1 == '--help' ]
+	then
+		show_help
+		exit 1
+	fi
+
+	DEVICE=$1
+
+	case $2 in
+		"step")
+			do_step $DEVICE $3
+			break
+			;;
+		"get_percent")
+			do_get_percent $DEVICE
+			break
+			;;
+		*)
+			show_help
+			;;
+	esac
 else
-	echo "Usage: brightness_control DEVICE STEP"
-	echo "Make sure to set udev rules to allow users of video group to control backlight:"
-	echo "    /etc/udev/rules.d/90-backlight.rules"
-	echo '    SUBSYSTEM=="backlight", ACTION=="add", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"'
+	show_help
 fi
