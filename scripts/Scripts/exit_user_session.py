8dbce49456ad668836a7669d7502f2615ba0407d
Kye Morton <pryre.dev@outlook.com>
2019-04-03 12:57:50 +1000

    Nicer logout handler

---
+++ scripts/Scripts/exit_user_session.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import sys
+import subprocess
+from PyQt5.QtWidgets import (QWidget, QPushButton, QLineEdit,
+							 QInputDialog, QApplication, QDialog)
+
+class MyPopup(QDialog):
+	def __init__(self):
+		super().__init__()
+		self.init_ui()
+
+class ExitUserSession(QDialog, QWidget):
+	def __init__(self):
+		super().__init__()
+		self.init_ui()
+
+	def init_ui(self):
+		self.setWindowTitle('Exit Session')
+		self.setGeometry(300, 300, 290, 150)
+
+		self.button_logout = QPushButton('Logout', self)
+		self.button_logout.move(20, 20)
+		self.button_logout.clicked.connect(self.do_logout)
+
+		self.button_shutdown = QPushButton('Shutdown', self)
+		self.button_shutdown.move(20, 60)
+		self.button_shutdown.clicked.connect(self.do_shutdown)
+
+		self.button_cancel = QPushButton('Cancel', self)
+		self.button_cancel.move(20, 100)
+		self.button_cancel.clicked.connect(self.do_cancel)
+
+	def do_logout(self):
+		subprocess.run(["swaymsg", "exit"])
+
+	def do_shutdown(self):
+		subprocess.run(["systemctl", "poweroff"])
+
+	def do_cancel(self):
+		self.close()
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+
+	window = ExitUserSession()
+	window.show()
+
+	sys.exit(app.exec_())
