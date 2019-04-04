89fb581376840306fb8f7e9c6df6f4a673e6576e
Kye Morton <pryre.dev@outlook.com>
2019-04-04 13:27:51 +1000

    Reboot and lock config

--- scripts/Scripts/exit_user_session.py
+++ scripts/Scripts/exit_user_session.py
@@ -17,22 +17,29 @@ class ExitUserSession(QDialog, QWidget):
 
 	def init_ui(self):
 		self.setWindowTitle('Exit Session')
-		self.setGeometry(300, 300, 120, 150)
+		self.setGeometry(300, 300, 120, 190)
 
 		self.button_logout = QPushButton('Logout', self)
 		self.button_logout.move(20, 20)
 		self.button_logout.clicked.connect(self.do_logout)
 
+		self.button_reboot = QPushButton('Reboot', self)
+		self.button_reboot.move(20, 60)
+		self.button_reboot.clicked.connect(self.do_reboot)
+
 		self.button_shutdown = QPushButton('Shutdown', self)
-		self.button_shutdown.move(20, 60)
+		self.button_shutdown.move(20, 100)
 		self.button_shutdown.clicked.connect(self.do_shutdown)
 
 		self.button_cancel = QPushButton('Cancel', self)
-		self.button_cancel.move(20, 100)
+		self.button_cancel.move(20, 140)
 		self.button_cancel.clicked.connect(self.do_cancel)
 
 	def do_logout(self):
 		subprocess.run(["swaymsg", "exit"])
+
+	def do_reboot(self):
+		subprocess.run(["systemctl", "reboot"])
 
 	def do_shutdown(self):
 		subprocess.run(["systemctl", "poweroff"])
