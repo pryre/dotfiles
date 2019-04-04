d6abf5527de4fc1af3cd7d08a2f371d57f1b1779
Kye Morton <pryre.dev@outlook.com>
2019-04-04 15:57:22 +1000

    Added mouse in nano, suspend to exit script and mute display to waybar

--- scripts/Scripts/exit_user_session.py
+++ scripts/Scripts/exit_user_session.py
@@ -17,26 +17,33 @@ class ExitUserSession(QDialog, QWidget):
 
 	def init_ui(self):
 		self.setWindowTitle('Exit Session')
-		self.setGeometry(300, 300, 120, 190)
+		self.setGeometry(300, 300, 120, 230)
 
 		self.button_logout = QPushButton('Logout', self)
 		self.button_logout.move(20, 20)
 		self.button_logout.clicked.connect(self.do_logout)
 
+		self.button_suspend = QPushButton('Suspend', self)
+		self.button_suspend.move(20, 60)
+		self.button_suspend.clicked.connect(self.do_suspend)
+
 		self.button_reboot = QPushButton('Reboot', self)
-		self.button_reboot.move(20, 60)
+		self.button_reboot.move(20, 100)
 		self.button_reboot.clicked.connect(self.do_reboot)
 
 		self.button_shutdown = QPushButton('Shutdown', self)
-		self.button_shutdown.move(20, 100)
+		self.button_shutdown.move(20, 140)
 		self.button_shutdown.clicked.connect(self.do_shutdown)
 
 		self.button_cancel = QPushButton('Cancel', self)
-		self.button_cancel.move(20, 140)
+		self.button_cancel.move(20, 180)
 		self.button_cancel.clicked.connect(self.do_cancel)
 
 	def do_logout(self):
 		subprocess.run(["swaymsg", "exit"])
+
+	def do_suspend(self):
+		subprocess.run(["systemctl", "suspend"])
 
 	def do_reboot(self):
 		subprocess.run(["systemctl", "reboot"])
