53c062e7cfd02786411fcff22014f256cd9d7d5a
Kye Morton <pryre.dev@outlook.com>
2019-04-12 12:01:00 +1000

    All the apps!

--- scripts/Scripts/list_desktop_applications.sh
+++ scripts/Scripts/list_desktop_applications.sh
@@ -3,4 +3,4 @@
 APP_DIR_SYSTEM=~/.local/share/applications/
 APP_DIR_LOCAL=/usr/share/applications/
 
-find $APP_DIR_SYSTEM $APP_DIR_LOCAL -maxdepth 1 -type f -type f -name '*.desktop' #-execdir basename '{}' ';' | awk -F '.desktop' ' { print $1}'
+find -L $APP_DIR_SYSTEM $APP_DIR_LOCAL -maxdepth 1 -type f -name '*.desktop' #-execdir basename '{}' ';' | awk -F '.desktop' ' { print $1}'
