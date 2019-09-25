9d03369698373650a45240dd859e960df8b2826e
Kye Morton <pryre.dev@outlook.com>
2019-09-25 10:45:19 +1000

    Fix for poor argument handling

--- scripts/Scripts/pdfExtract.sh
+++ scripts/Scripts/pdfExtract.sh
@@ -5,21 +5,21 @@ extract_pages() {
 
 		gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER \
 			-dFirstPage=$2 -dLastPage=$3 \
-			-sOutputFile="split-$2-$3.pdf" $1
+			-sOutputFile="split-$2-$3.pdf" "$1"
 }
 
 case "$#" in
 	1)
 		#Extract all pages
-			PG_NUM=$(gs -q -dNODISPLAY -c \
-				"($1) (r) file runpdfbegin pdfpagecount = quit")
+		PG_NUM=$(gs -q -dNODISPLAY -c \
+				 "($1) (r) file runpdfbegin pdfpagecount = quit")
 
 		for i in $(seq -w $PG_NUM); do
-			extract_pages $1 $i $i
+			extract_pages "$1" "$i" "$i"
 		done
 	;;
 	3)
-		extract_pages $1 $2 $3
+		extract_pages "$1" "$2" "$3"
 	;;
 	*)
 		echo "Usage: pdfExtract.sh FILE [start end]"
