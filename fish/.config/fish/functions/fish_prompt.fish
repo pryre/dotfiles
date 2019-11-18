42d0feb8267f186756d3821b19cd128403684240
Kye Morton <pryre.dev@outlook.com>
2019-11-18 13:07:47 +1000

    Updated prompt and added sudo-write tool

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,8 @@
 function fish_prompt
-		set chost (set_color white)$USER@(prompt_hostname)(set_color normal)
+		set chost (set_color white)'['(set_color normal)$USER'@'(prompt_hostname)(set_color white)']'(set_color normal)
 		set cdir (set_color green)(prompt_pwd_short)(set_color normal)
+		set cprompt (set_color white)"> "(set_color normal)
 
-	    echo [$chost] $cdir"> "
+	    echo $chost' '$cdir$cprompt
 end
 
