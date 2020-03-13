9e190fdd4bb678b595ccfd0d75e9575f58cb72e7
Kye Morton <pryre.dev@outlook.com>
2020-03-14 07:49:24 +1000

    Should have always been a cache dir, not config

--- scripts/Scripts/swaylock-blur.sh
+++ scripts/Scripts/swaylock-blur.sh
@@ -1,40 +1,37 @@
 #!/bin/sh
 
-if [ -z $XDG_CONFIG_HOME ]
+if [ -z $XDG_CACHE_HOME ]
 then
-	XDG_CONFIG_HOME=$HOME/.config
+	XDG_CACHE_HOME=$HOME/.cache
 fi
 
 NBG="$HOME/.background"
-BBG="$XDG_CONFIG_HOME/swaylock/blur_background.png"
+BBG="$XDG_CACHE_HOME/swaylock/blur_background.png"
 
 RECALC=
-if [ ! -e "$BBG" ]
+if [ ! -e "$BBG" ] || [ ! -d $(dirname $BBG) ]
 then
 	RECALC=1
-	echo not there
+	mkdir -p $(dirname $BBG)
 else
+	# Does exist, but we need a newer copy
 	if [ "$(($(date -r "$NBG" +%s)-$(date -r "$BBG" +%s)))" -gt 0 ]
 	then
 		RECALC=1
-		echo newer
 	fi
 fi
 
 if [ "$RECALC" ]
 then
-	echo blurring
+	# Many different options for performing the function
+	# Scaling is used to speed up locking process
+	#
     # cp -L $NBG $BBG
-	# convert "$NBG" -filter Gaussian -blur 0x4 "$BBG"
+	# grim "$BBG"; convert "$BBG" -scale 10% -scale 1000% "$BBG" # Should be done per output
+    # convert "$NBG" -filter Gaussian -blur 0x4 "$BBG"
 	# convert "$NBG" -scale 10% -scale 1000% "$BBG"
-	# convert $BBG -scale 25%
-	# convert $BBG -blur 0x8 $BBG
-	#Then put back to 200% to get back to orginal size
-	# convert $BBG -scale 2.5%
+
 	convert "$NBG" -scale 25% -blur 0x2 -scale 400% "$BBG"
-	# convert "$NBG" -scale 50% "$BBG"
-	# convert $NBG -blur 0x8 $BBG
-	# convert "$BBG" -scale 200% "$BBG"
 fi
 
 swaylock -i "$BBG" $@
