167744f193b73366e6dc658ca819083ba8a576f7
Kye Morton <pryre.dev@outlook.com>
2020-03-18 08:49:32 +1000

    Lock icon looks nice

--- scripts/Scripts/swaylock-blur.sh
+++ scripts/Scripts/swaylock-blur.sh
@@ -7,6 +7,8 @@ fi
 
 NBG="$HOME/.background"
 BBG="$XDG_CACHE_HOME/swaylock/blur_background.png"
+LIO="$HOME/.local/share/icons/la-capitaine-icon-theme/status/symbolic/changes-prevent-symbolic.svg"
+LIO_SIZE="200x200"
 
 RECALC=
 if [ ! -e "$BBG" ]
@@ -32,6 +34,22 @@ then
 	# convert "$NBG" -scale 10% -scale 1000% "$BBG"
 
 	convert "$NBG" -scale 25% -blur 0x2 -scale 400% "$BBG"
+
+	# Overlay a locking icon if set/exists
+	# If it's an .svg we have to have a density setting to make sure it scales well
+	if [ "$LIO" ] && [ -e "$LIO" ]
+	then
+		LIR="$XDG_CACHE_HOME/swaylock/lock_icon.png"
+		case "$LIO" in
+			*.svg)
+				convert -background none -density 1200 -resize "$LIO_SIZE" "$LIO" "$LIR"
+				;;
+			*)
+				convert -background none -resize "$LIO_SIZE" "$LIO" "$LIR"
+				;;
+		esac
+		composite -gravity center "$LIR" "$BBG" "$BBG"
+	fi
 fi
 
 swaylock -i "$BBG" $@
