95dfaa6cd5f45006ef902ea0b0592ff8445f48de
Kye Morton <pryre.dev@outlook.com>
2020-08-12 07:38:02 +1000

    Many inputs!

--- scripts/Scripts/fix16_converter.py
+++ scripts/Scripts/fix16_converter.py
@@ -9,18 +9,20 @@ def Q_to_float(q, bits):
 def float_to_Q(f, bits):
     return int(f * pow(2, bits));
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
 	print("Usage:")
-	print("    ./float_to_fix16.py FLOAT")
-	print("    ./float_to_fix16.py 0xHEX")
+	print("    ./float_to_fix16.py FLOAT ...")
+	print("    ./float_to_fix16.py 0xHEX ...")
 	exit(1)
 
-# If hex -> float
-if('x' in sys.argv[1] ) or ('X' in sys.argv[1]):
-	val = int(sys.argv[1],16)
-	f = Q_to_float(val, 16)
-	print("%s -> %0.8f" % ("0x" + format(val, 'X').rjust(8, '0'), f))
-else: # If float -> hex
-	val = float(sys.argv[1])
-	q = float_to_Q(val, 16)
-	print("%0.8f -> %0.8f (%s)" % (val, Q_to_float(q,16), "0x" + format(q, 'X').rjust(8, '0')))
+for num in sys.argv[1:]:
+	# If hex -> float
+	if('x' in num ) or ('X' in num):
+		val = int(num,16)
+		f = Q_to_float(val, 16)
+		print("%s -> %0.8f" % ("0x" + format(val, 'X').rjust(8, '0'), f))
+	else: # If float -> hex
+		val = float(num)
+		q = float_to_Q(val, 16)
+		print("%0.8f -> %0.8f (%s)" % (val, Q_to_float(q,16), "0x" + format(q, 'X').rjust(8, '0')))
+
