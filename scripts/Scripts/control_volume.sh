5c7389c968d46fa1d03d3c9d93c26a0389d4fe9b
Kye Morton <pryre.dev@outlook.com>
2021-01-24 15:09:41 +1000

    Removed references to pacmd

--- scripts/Scripts/control_volume.sh
+++ scripts/Scripts/control_volume.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 COMMAND=$1
+SINK_LIST=$(pactl list sinks)
 SINKS=""
 if [ $# -ge 2 ]
 then
 	if [ $2 = '-a' ]
 	then
 		# Use all sinks if flagged
-		SINKS=$(pacmd list-sinks | tr \* ' ' | awk '/index:/{print $2}')
+		SINKS=$(echo "$SINK_LIST" | grep -e 'Sink #' | cut -d '#' -f 2) # tr \* ' ' | awk '/Sink #/{print $2}')
 	else
 		# Use list of sinks if provided
 		shift
@@ -15,11 +16,15 @@ then
 	fi
 else
 	# Grab first running sink if we have one
-	RUNSINK=$(pacmd list-sinks | grep -e index -e state | sed -n '/RUNNING/{x;p;d;}; x' | head -n 1 | cut -d':' -f2)
+	RUNSINK=$(echo "$SINK_LIST" | grep -e 'Sink #' -e 'State' | sed -n '/RUNNING/{x;p;d;}; x' | head -n 1 | cut -d'#' -f2)
 	if [ -z "$RUNSINK" ]
 	then
 		# No match, use default sink if none provided
-		SINKS=$(pacmd list-sinks | grep '*' | tr \* ' ' | awk '/index:/{print $2}')
+		# XXX: This probably doesn't work
+		# SINKS=$(echo "$SINK_LIST" | grep '*' | grep -e 'Sink #' | cut -d '#' -f 2)
+
+		# Try get an idle or suspended sink instead
+		SINKS=$(echo "$SINK_LIST" | grep -e 'Sink #' -e 'State' | sed -n -E '/SUSPENDED|IDLE/{x;p;d;}; x' | head -n 1 | cut -d'#' -f2)
 	else
 		# There was a match, so use that
 		SINKS=$RUNSINK
@@ -51,16 +56,18 @@ do_cmd_many() {
 
 do_get_volume_many() {
 	SINKS=$@
-	SINKS_DETAILS=$(pacmd list-sinks | \
-				  grep -e 'index' -e 'volume:' -e 'device.description' -e 'muted' | \
-				  grep -e 'base volume' -v)
+	SINKS_DETAILS=$(pactl list sinks | \
+					grep -e 'Sink #' -e 'Volume' -e 'node.nick' -e 'Mute' | \
+					grep -e 'Base Volume' -v)
+				  # grep -e 'index' -e 'volume:' -e 'node.nick' -e 'muted' | \
+				  # grep -e 'base volume' -v)
 
 	for SINK in $SINKS
 	do
-		SINK_DETAILS=$(echo "$SINKS_DETAILS" | grep -e "index: $SINK" -A 3 | grep -e index -v)
-		SINK_VOLUME=$(echo "$SINK_DETAILS" | grep "volume" | awk '{print $5}' | sed -e 's/%//')
-		SINK_MUTED=$(echo "$SINK_DETAILS" | grep "muted: yes")
-		SINK_NAME=$(echo "$SINK_DETAILS" | grep "description" | cut -d '"' -f 2 | cut -d '[' -f 1 | cut -d '(' -f 1 | sed 's/[[:space:]]*$//')
+		SINK_DETAILS=$(echo "$SINKS_DETAILS" | grep -e "Sink #$SINK" -A 3 | grep -e "Sink #" -v)
+		SINK_VOLUME=$(echo "$SINK_DETAILS" | grep -e "Volume" | cut -d '/' -f 2 | sed -e 's/^[[:space:]]*//' -e 's/[[:blank:]]*$//' -e 's/%//')
+		SINK_MUTED=$(echo "$SINK_DETAILS" | grep "Mute: yes")
+		SINK_NAME=$(echo "$SINK_DETAILS" | grep "node.nick" | cut -d '=' -f 2 | sed -e 's/^[[:space:]]*//' -e 's/[[:blank:]]*$//' -e 's/"//g') #  cut -d '[' -f 1 | cut -d '(' -f 1 | sed 's/[[:space:]]*$//')
 
 		if [ "$SINK_VOLUME" -eq 0 ] || [ "$SINK_MUTED" ]
 		then
@@ -73,7 +80,7 @@ do_get_volume_many() {
 
 send_notify() {
 	SINKS=$@
-	echo
+
 	if [ $# -gt 1 ]; then
 		VOLUMES=$(do_get_volume_many $SINKS)
 
@@ -93,7 +100,7 @@ send_notify() {
 				;;
 		esac
 
-		echo $(printf "%.0f\n" $SVOL)
+		# echo $(printf "%.0f\n" $SVOL)
 
 		notify-send.sh  "$VOLUMES" \
 						-t 2000 \
@@ -107,19 +114,19 @@ send_notify() {
 
 case $COMMAND in
 	"toggle_mute")
-		#pactl set-sink-mute $SINKS toggle
+		#pactl set-sink-mute $SINK toggle
 		do_cmd_many "pactl set-sink-mute" "toggle" $SINKS
 		send_notify $SINKS
 		break
 		;;
 	"lower")
-		#pactl set-sink-volume $SINKS -5%
+		#pactl set-sink-volume $SINK -5%
 		do_cmd_many "pactl set-sink-volume" "-5%" $SINKS
 		send_notify $SINKS
 		break
 		;;
 	"raise")
-		#pactl set-sink-volume $SINKS +5%
+		#pactl set-sink-volume $SINK +5%
 		do_cmd_many "pactl set-sink-volume" "+5%" $SINKS
 		send_notify $SINKS
 		break
