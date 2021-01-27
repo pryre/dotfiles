049fe805d6f52372c3e2ad871533cd71e41f3e23
Kye Morton <pryre.dev@outlook.com>
2021-01-27 19:21:11 +1000

    Support for autocomplete in fish

--- fish/.config/fish/functions/foxy_ros.fish
+++ fish/.config/fish/functions/foxy_ros.fish
@@ -1,8 +1,15 @@
 function foxy_ros
-     # set -g -x ROS_DOMAIN_ID 42
-     set -g -x ROS_VERSION 2
-     set -g -x ROS_PYTHON_VERSION 3
-     set -g -x ROS_DISTRO foxy
-     bass source /opt/ros2/foxy/setup.bash
-     bass source ~/Workspace/ros2_dev_ws/install/local_setup.bash
+	# set -g -x ROS_DOMAIN_ID 42
+	set -g -x ROS_VERSION 2
+	set -g -x ROS_PYTHON_VERSION 3
+	set -g -x ROS_DISTRO foxy
+	bass source /opt/ros2/foxy/setup.bash
+	bass source ~/Workspace/ros2_dev_ws/install/local_setup.bash
+
+	# Autocompletion
+	# XXX: This could be taken out later after the following pull request
+	# https://github.com/ros2/ros2cli/pull/326
+	if type register-python-argcomplete > /dev/null 2>&1
+		eval "register-python-argcomplete --shell fish ros2 | source"
+	end
 end
