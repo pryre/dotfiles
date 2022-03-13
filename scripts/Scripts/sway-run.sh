0353760675a5cb0c2a646df33c15fbf703abb9f3
Kye Morton <pryre.dev@outlook.com>
2022-03-13 19:39:55 +1000

    Added sway env files

---
+++ scripts/Scripts/sway-run.sh
@@ -0,0 +1,9 @@
+# Session
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
+
+source /usr/local/bin/wayland_enablement.sh
+
+systemd-cat --identifier=sway sway $@
+
